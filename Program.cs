9697e1d4740c485f6f8a732240448b5e246240b9
Ablatytu <stassuverin@gmail.com>
2023-12-23 21:06:21 +0300

    Начало программы

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string input = Console.ReadLine();
+
+        // Разбиваем введенную строку на массив строк
+        string[] inputArray = input.Split(',');
+
+        // Вызываем метод, который формирует новый массив из строк, длина которых <= 3 символам
+        string[] resultArray = FilterStrings(inputArray);
+
+        // Выводим результат
+        Console.WriteLine("Результат:");
+        Console.WriteLine(string.Join(", ", resultArray));
+    }
+}
